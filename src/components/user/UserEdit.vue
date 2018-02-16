5bb3ef6a6314b202e3c7b6d8734017ec91edce42
Rio Oktafianto <rioookta@gmail.com>
2018-02-16 09:32:09 +0700

    First Commit

---
+++ src/components/user/UserEdit.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3>Edit the User</h3>
+        <p>Locale: {{ $route.query.locale }}</p>
+        <p>Analytics: {{ $route.query.q }}</p>
+        <button class="btn btn-primary" @click="confirmed = true">Confirm</button>
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                confirmed: false
+            }
+        },
+        beforeRouteLeave(to, from, next){
+            if(this.confirmed) {
+                next();
+            }else{
+                if(confirm('Are you Sure?')){
+                    next();
+                }else{
+                    next(false);
+                }
+            }
+        }
+    }
+</script>
